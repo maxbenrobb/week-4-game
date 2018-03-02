3cd784f986e83d681dbbbb221ade99683c63598d
Max Robb <maxbenrobb@maxs-mbp.attlocal.net>
2018-03-01 22:37:44 -0500

    file update 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,15 @@
-var targetNumber = $("#random-number").append(Math.floor(Math.random()*120));
-
-  var crystals = $("#crystals");
-
+var targetNumber = $("#random-number").append(Math.floor(Math.random()*100)+19);
+var crystals = [$("crystal1"), $("crystal2"), $("crystal3"), $("crystal4")];
   var wins = 0;
   var losses = 0;
 
-  // Now for the hard part. Creating multiple crystals each with their own unique number value.
-
-  // We begin by expanding our array to include four options.
-  var numberOptions = [10, 5, 3, 7];
+  // value for crystal, need to make 4 integers to assign to the 4 crystals
+  var numberOptions = $("#crystal-value").text(Math.floor(Math.random()*20));
+  console.log(numberOptions);
 
 
   // This time, our click event applies to every single crystal on the page. Not just one.
-  crystals.on("click", ".crystal-image", function() {
+  crystals.click("crystals", function() {
 
     // Determining the crystal's value requires us to extract the value from the data attribute.
     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
@@ -21,19 +18,31 @@ var targetNumber = $("#random-number").append(Math.floor(Math.random()*120));
 
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
+
     // We then add the crystalValue to the user's "counter" which is a global variable.
     // Every click, from every crystal adds to the global counter.
     counter += crystalValue;
 
     // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + counter);
+
 
     if (counter === targetNumber) {
       alert("You win!");
+      wins++;
+      gameReset();
     }
 
     else if (counter >= targetNumber) {
       alert("You lose!!");
+      losses++;
+      gameReset();
     }
 
   });
+
+  function gameReset() {
+    counter = "";
+    computerGuess = getNewComputerGuess();
+    document.getElementById("guess-counter").innerHTML = "";
+    document.getElementById("guess-left").innerHTML = guessLeft;
+}
