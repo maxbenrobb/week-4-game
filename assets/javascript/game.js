f175d7445aab243bbd72712dda501c9031b6812e
Max Robb <maxbenrobb@maxs-mbp.attlocal.net>
2018-03-02 20:08:07 -0500

    final fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,51 @@
-var targetNumber = $("#random-number").append(Math.floor(Math.random()*100)+19);
-var crystals = [$("crystal1"), $("crystal2"), $("crystal3"), $("crystal4")];
-  var wins = 0;
-  var losses = 0;
+var numberToGuess;
+var crystal1;
+var crystal2;
+var crystal3;
+var crystal4;
+var totalScore;
+var wins = 0;
+var losses = 0;
 
-  // value for crystal, need to make 4 integers to assign to the 4 crystals
-  var numberOptions = $("#crystal-value").text(Math.floor(Math.random()*20));
-  console.log(numberOptions);
+$(document).ready(function() {
 
+  resetGame();
 
-  // This time, our click event applies to every single crystal on the page. Not just one.
-  crystals.click("crystals", function() {
+  $('#crystal1, #crystal2, #crystal3, #crystal4').click(function(e){
+    var imageId = e.target.id;
 
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+    if(imageId === 'crystal1') {
+      totalScore += crystal1;
+    } else if(imageId == 'crystal2') {
+      totalScore += crystal2;
+    } else if(imageId == 'crystal3') {
+      totalScore += crystal3;
+    } else if(imageId == 'crystal4') {
+      totalScore += crystal4;
+    }
+    $('#total-score').text(totalScore);
 
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
-
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-
-
-    if (counter === targetNumber) {
-      alert("You win!");
+    if(totalScore === numberToGuess) {
       wins++;
-      gameReset();
-    }
+      $('#wins-counter').text(wins);
+      resetGame();
 
-    else if (counter >= targetNumber) {
-      alert("You lose!!");
+    } else if(totalScore > numberToGuess) {
       losses++;
-      gameReset();
+      $('#losses-counter').text(losses);
+      resetGame();
     }
+   });
+});
 
-  });
+function resetGame() {
+  numberToGuess = Math.floor(Math.random()*100)+19;
+  crystal1 = Math.floor(Math.random()*20 + 1);
+  crystal2 = Math.floor(Math.random()*20 + 1);
+  crystal3 = Math.floor(Math.random()*20 + 1);
+  crystal4 = Math.floor(Math.random()*20 + 1);
+  totalScore = 0;
 
-  function gameReset() {
-    counter = "";
-    computerGuess = getNewComputerGuess();
-    document.getElementById("guess-counter").innerHTML = "";
-    document.getElementById("guess-left").innerHTML = guessLeft;
+  $('#total-score').text(totalScore);
+  $('#number2guess').text(numberToGuess);
 }
